a737649845af31356ba96b31fc39076cf41439fc
Saurabh <61317144+0xSaurabhx@users.noreply.github.com>
2024-04-25 19:29:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
 load_dotenv() 
 
-openai.api_key = "ENTER_YOUR_KEY"
+openai.api_key = os.environ['OPENAI_KEY']
 
 llm_name = "gpt-3.5-turbo"
 qna_chain = None
